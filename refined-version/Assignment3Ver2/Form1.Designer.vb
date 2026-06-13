ebc7e1e3d8c98b3442aca6c2cde8c8d71e6a2bd5
GSM-07 <11195025@college.wlc.ac.uk>
2026-06-13 23:11:09 +0800

    Refined version after testing
     corrected exchange rate model

---
+++ refined-version/Assignment3Ver2/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.ddlFromCurrency = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.ddlToCurrency = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblRateUsed = New System.Windows.Forms.Label()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(25, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(201, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Currency Converter"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(115, 157)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(196, 20)
+        Me.txtAmount.TabIndex = 2
+        '
+        'ddlFromCurrency
+        '
+        Me.ddlFromCurrency.FormattingEnabled = True
+        Me.ddlFromCurrency.Location = New System.Drawing.Point(56, 96)
+        Me.ddlFromCurrency.Name = "ddlFromCurrency"
+        Me.ddlFromCurrency.Size = New System.Drawing.Size(121, 21)
+        Me.ddlFromCurrency.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(112, 141)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Enter Amount "
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(133, 268)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(163, 23)
+        Me.btnConvert.TabIndex = 7
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(53, 324)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(7, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(112, 202)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(39, 13)
+        Me.lblResult.TabIndex = 9
+        Me.lblResult.Text = "Label5"
+        '
+        'ddlToCurrency
+        '
+        Me.ddlToCurrency.FormattingEnabled = True
+        Me.ddlToCurrency.Location = New System.Drawing.Point(229, 96)
+        Me.ddlToCurrency.Name = "ddlToCurrency"
+        Me.ddlToCurrency.Size = New System.Drawing.Size(121, 21)
+        Me.ddlToCurrency.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(53, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "From:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(226, 80)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "To:"
+        '
+        'lblRateUsed
+        '
+        Me.lblRateUsed.AutoSize = True
+        Me.lblRateUsed.Location = New System.Drawing.Point(112, 230)
+        Me.lblRateUsed.Name = "lblRateUsed"
+        Me.lblRateUsed.Size = New System.Drawing.Size(39, 13)
+        Me.lblRateUsed.TabIndex = 11
+        Me.lblRateUsed.Text = "Label6"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 574)
+        Me.Controls.Add(Me.lblRateUsed)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ddlToCurrency)
+        Me.Controls.Add(Me.ddlFromCurrency)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents ddlFromCurrency As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents ddlToCurrency As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblRateUsed As Label
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+End Class
---
+++ refined-version/Assignment3Ver2/Form1.vb
@@ -0,0 +1,126 @@
+﻿'currency converter - refined 
+Imports System.Security
+
+Public Class Form1
+
+    'list of supported currencies shown in both combo boxes'
+    Dim supportedCurrencies = {"EUR", "USD", "JPY", "GBP"}
+
+    'fixed after testing:
+    ' rates now use the correct model: "1 GBP = 1.15 EUR, 1 GBP = 1.26 USD, 1 GBP = 200 JPY"
+    ' this ensure that the conversion formula works correctly for all currency pairs.
+    Dim currentRates As New Dictionary(Of String, Double) From {
+     {"EUR", 1.15},
+     {"USD", 1.26},
+     {"JPY", 200.0},
+     {"GBP", 1.0}
+    }
+
+    'currency symbol dictionary 
+    'added after testing to display the correct symbol for the converted amount.
+    Dim currencySymbols As New Dictionary(Of String, String) From {
+     {"GBP", "£"},
+     {"EUR", "€"},
+     {"USD", "$"},
+     {"JPY", "¥"}
+    }
+
+    ' Form laod - initial setup '
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'fill both combo boxes with the same list of currencies'
+        ddlFromCurrency.Items.AddRange(supportedCurrencies)
+        ddlToCurrency.Items.AddRange(supportedCurrencies)
+
+        ' default selections '
+        ddlFromCurrency.SelectedItem = "GBP"       ' user enters amount in GBP'
+        ddlToCurrency.SelectedItem = "EUR"         ' convert into EUR '
+
+        'clear UI fields'
+        txtAmount.Text = ""
+        lblResult.Text = ""
+        lblRateUsed.Text = ""
+    End Sub
+
+    'when user changes the "from" currency'
+    Private Sub ddlFromCurrency_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlFromCurrency.SelectedIndexChanged
+
+        'clear amount and result because conversion is no longer valid'
+        txtAmount.Text = ""
+        lblResult.Text = ""
+    End Sub
+
+    Private Sub ddlToCurrency_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlToCurrency.SelectedIndexChanged
+
+        'clear result because conversion must be recalculated'
+        lblResult.Text = ""
+    End Sub
+
+    'validate amount as user types'
+    Private Sub txtAmount_TextChanged(sender As Object, e As EventArgs) Handles txtAmount.TextChanged
+
+        'check if the amount is valid (numeric, >0, 2 decimals)'
+        If Not ValidAmount(txtAmount.Text) Then
+            ErrorProvider1.SetError(txtAmount, "Enter a positive number up to 2 decimals")
+            btnConvert.Enabled = False
+            Exit Sub
+        End If
+
+        ' Clear error and enable Convert button
+        ErrorProvider1.Clear()
+        btnConvert.Enabled = True
+    End Sub
+
+    'CONVERT BUTTON - MAIN LOGIC'
+    Private Sub btnConvert_click(sender As Object, e As EventArgs) Handles btnConvert.Click
+
+        'validation check before converting '
+        If Not ValidAmount(txtAmount.Text) Then
+            ErrorProvider1.SetError(txtAmount, "Enter a positive number up to 2 decimals")
+            Exit Sub
+        End If
+
+        ErrorProvider1.Clear()
+
+        ' convert input text to a number 
+        Dim amount As Double = Double.Parse(txtAmount.Text)
+
+        'read selected currencies 
+        Dim fromCurrency As String = ddlFromCurrency.SelectedItem.ToString()
+        Dim toCurrency As String = ddlToCurrency.SelectedItem.ToString()
+
+        'get their rates from the dictionary 
+        Dim fromRate As Double = currentRates(fromCurrency)
+        Dim toRate As Double = currentRates(toCurrency)
+
+        'CONVERSION FORMULA (amount x (toRate / fromRate))
+        Dim convertedAmount As Double =
+            Math.Round(amount * (toRate / fromRate), 2)
+
+        'correct symbol for the converted currency and display result (added after testing)
+        Dim symbol As String = currencySymbols(toCurrency)
+        lblResult.Text = symbol & convertedAmount.ToString("n2")
+
+        ' show exchange rate used in both directions 
+        lblRateUsed.Text =
+            $"1 {fromCurrency} = {(fromRate / toRate):F4} {toCurrency} | " &
+            $"1 {toCurrency} = {(toRate / fromRate):F4} {fromCurrency}"
+
+    End Sub
+
+    'validate function for amount 
+    Private Function ValidAmount(s As String) As Boolean
+
+        'check numeric
+        Dim v As Double
+        If Not Double.TryParse(s, v) Then Return False
+
+        'must be positive and not exceedingly large
+        If v <= 0 Or v > 1000000 Then Return False
+
+        'check decimal places (max 2 )
+        Dim decimals = BitConverter.GetBytes(Decimal.GetBits(CDec(v))(3))(2)
+        Return decimals <= 2
+    End Function
+
+End Class
